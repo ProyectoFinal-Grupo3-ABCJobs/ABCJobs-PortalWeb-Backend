f44afd1a596dc187e5b350518d7f7fc62b3c1702
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-10-15 18:05:22 -0500

    bd y código inicial registro candidato

---
+++ gestion_candidatos/modelo/modeloCandidato.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class Candidato(db.Model):
+    idCandidato         = db.Column(db.Integer, primary_key = True)
+    tipoIdentificacion  = db.Column(db.String(50))
+    identificacion      = db.Column(db.String(50))
+    nombre              = db.Column(db.String(150))
+    direccion           = db.Column(db.String(150))
+    telefono            = db.Column(db.Integer)
+    profesion           = db.Column(db.String(150))
+    aniosExperiencia    = db.Column(db.String(150))
+    email               = db.Column(db.String(150))
+    idCiudad            = db.Column(db.String(100))
+    contrasena          = db.Column(db.String(50))
+    ultimoEstudio       = db.Column(db.String(150))
+    institucion         = db.Column(db.String(150))
+    anioGrado           = db.Column(db.DateTime())
+    cargoUltimoEmpleo   = db.Column(db.String(150))
+    empresa             = db.Column(db.String(150))
+    anioIngreso         = db.Column(db.DateTime())
+    anioRetiro          = db.Column(db.DateTime())
+    estado              = db.Column(db.Boolean, default=False)
+
+class CandidatoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Candidato
+        load_instance = True
---
+++ gestion_candidatos/vista/__init__.py
@@ -0,0 +1,1 @@
+from .vista_candidato import *

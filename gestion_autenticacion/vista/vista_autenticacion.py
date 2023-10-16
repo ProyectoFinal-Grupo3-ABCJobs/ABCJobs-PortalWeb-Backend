342d9bfb199ca69c67f42e6972f34e7d660ac2d1
gcastrot82 <gcastrot82@hotmail.com>
2023-10-15 22:45:52 -0500

    Update: ajuste retorno de autenticacion en el codigo

--- gestion_autenticacion/vista/vista_autenticacion.py
+++ gestion_autenticacion/vista/vista_autenticacion.py
@@ -61,7 +61,7 @@ class VistaGenerarToken(Resource):
           # Usuario autenticado
           mensaje:dict = {"id": usuario.id, "tipoUsuario": usuario.tipoUsuario,"token": token_de_acceso}
           respuesta = jsonify(mensaje)
-          respuesta.status_code = 401
+          respuesta.status_code = 200
           return respuesta
  
 

8621244541efaf8b1d96876efadfc8edf859d148
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-10-15 19:56:55 -0500

    ajuste registro candidato

--- gestion_candidatos/vista/vista_candidato.py
+++ gestion_candidatos/vista/vista_candidato.py
@@ -4,21 +4,16 @@ from flask_jwt_extended import create_access_token
 from flask import jsonify
 import hashlib, os, json
 
-from modelo import Candidato, CandidatoSchema
+from modelo import db, Candidato, CandidatoSchema
 candidate_schema = CandidatoSchema()
 
- class VistaRegistroCandidato(Resource):
+class VistaRegistroCandidato(Resource):
     def post(self):
 
           try:
-                if len(request.json["tipoIdentificacion"].strip())==0 or len(request.json["identificacion"].strip())==0 or len(request.json["nombre"].strip())==0 /
-                 or len(request.json["direccion"].strip())==0 or len(request.json["telefono"].strip())==0 or len(request.json["profesion"].strip())==0 /
-                 or len(request.json["aniosExperiencia"].strip())==0 or len(request.json["email"].strip())==0 or len(request.json["idCiudad"].strip())==0 /
-                 or len(request.json["contrasena"].strip())==0 or len(request.json["ultimoEstudio"].strip())==0 or len(request.json["institucion"].strip())==0 /
-                 or len(request.json["anioGrado"].strip())==0 or len(request.json["cargoUltimoEmpleo"].strip())==0 or len(request.json["empresa"].strip())==0 /
-                 or len(request.json["anioIngreso"].strip())==0 or len(request.json["anioRetiro"].strip())==0:
+                if len(request.json["tipoIdentificacion"].strip())==0 or len(request.json["identificacion"].strip())==0 or len(request.json["nombre"].strip())==0 or len(request.json["direccion"].strip())==0 or len(request.json["telefono"].strip())==0 or len(request.json["profesion"].strip())==0 or len(request.json["aniosExperiencia"].strip())==0 or len(request.json["email"].strip())==0 or len(request.json["idCiudad"].strip())==0 or len(request.json["contrasena"].strip())==0 or len(request.json["ultimoEstudio"].strip())==0 or len(request.json["institucion"].strip())==0 or len(request.json["anioGrado"].strip())==0 or len(request.json["cargoUltimoEmpleo"].strip())==0 or len(request.json["empresa"].strip())==0 or len(request.json["anioIngreso"].strip())==0 or len(request.json["anioRetiro"].strip())==0:
                     return "Code 400: Hay campos obligatorios vacíos", 400
-            except:
+          except:
                 return "Code 400: Hay campos obligatorios vacíos", 400
 
           nuevo_candidato = Candidato(

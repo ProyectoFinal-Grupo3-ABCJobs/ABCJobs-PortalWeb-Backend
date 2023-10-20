44a40414195220dd5ce340855de07c828468110c
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-10-19 22:28:45 -0500

    Update: actualizacion pruebas unitarias gestion empresas

--- gestion_empresas/app.py
+++ gestion_empresas/app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from datetime import timedelta
 from dotenv import load_dotenv
 from gestion_empresas.vista.vista_empresas import VistaSaludServicio,VistaRegistroEmpresa
-from modelo import db, EmpresaSchema
+from gestion_empresas.modelo import db, EmpresaSchema
 import os
 
 empresa_schema = EmpresaSchema()

89027f8c67779095569f3e93f8d130d89d0d21d9
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-10-22 02:59:37 -0500

    Fix01: Corregir problema de import para los unittest

--- gestion_empresas/app.py
+++ gestion_empresas/app.py
@@ -4,9 +4,19 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from datetime import timedelta
 from dotenv import load_dotenv
-from gestion_empresas.vista.vista_empresas import VistaSaludServicio,VistaRegistroEmpresa
-from gestion_empresas.modelo import db, EmpresaSchema
+
 import os
+
+directorio_actual = os.getcwd()
+carpeta_actual = os.path.basename(directorio_actual)
+
+if carpeta_actual=='gestion_empresas':
+    from vista.vista_empresas import VistaSaludServicio,VistaRegistroEmpresa
+    from modelo import db, EmpresaSchema
+else:    
+    from gestion_empresas.vista.vista_empresas import VistaSaludServicio,VistaRegistroEmpresa
+    from gestion_empresas.modelo import db, EmpresaSchema
+
 
 empresa_schema = EmpresaSchema()
 

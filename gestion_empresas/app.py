0e478dad04eed1bc63e10d1938f0c5d9298caf5e
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-10-26 19:01:17 -0500

    Update: ajuste import

--- gestion_empresas/app.py
+++ gestion_empresas/app.py
@@ -31,6 +31,7 @@ app.config["JWT_ALGORITHM"] = "HS256"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.config['PROPAGATE_EXEPTIONS'] = True
 
+
 app_context = app.app_context()
 app_context.push()
 

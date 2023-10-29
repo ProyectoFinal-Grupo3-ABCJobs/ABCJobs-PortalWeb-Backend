0c59d042d36d289006cf91d87df53ae6688f69cf
gcastrot82 <gcastrot82@hotmail.com>
2023-10-29 11:49:35 -0500

    Update: Se agregan funcionalidades para payload del Token, y modelo de empresas y autenticacion

--- gestion_empresas/modelo/modeloempresas.py
+++ gestion_empresas/modelo/modeloempresas.py
@@ -11,14 +11,29 @@ class Empresa(db.Model):
     direccion   = db.Column(db.String(100))
     telefono    = db.Column(db.String(20))
     idCiudad    = db.Column(db.Integer)
-    # proyectos   = db.relationship('Proyecto', cascade='all, delete, delete-orphan')   
+    idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), unique=True, nullable=True)
+    usuario = db.relationship('Usuario', back_populates='empresa', uselist=False)
     
+
 class Proyecto(db.Model):
     idProyecto          = db.Column(db.Integer, primary_key = True)
     nombreProyecto      = db.Column(db.String(100))
     numeroColaboradores = db.Column(db.String(20))
     fechaInicio         = db.Column(Date)
-    empresa_id = db.Column(db.Integer, db.ForeignKey("empresa.idEmpresa"), nullable=False)
+    empresa_id          = db.Column(db.Integer, db.ForeignKey("empresa.idEmpresa"), nullable=False)
+
+class Usuario(db.Model):
+    id          = db.Column(db.Integer, primary_key = True)
+    usuario     = db.Column(db.String(50))
+    contrasena  = db.Column(db.String(90))
+    tipoUsuario = db.Column(db.String(100))
+    empresa = db.relationship('Empresa', back_populates='usuario', uselist=False)
+
+    
+
+# Establecer la relación uno a uno entre Usuario y Empresa
+Usuario.empresa = db.relationship('Empresa', back_populates='usuario', uselist=False)
+Empresa.usuario = db.relationship('Usuario', back_populates='empresa')
 
 
 class EmpresaSchema(SQLAlchemyAutoSchema):
@@ -29,4 +44,9 @@ class EmpresaSchema(SQLAlchemyAutoSchema):
 class ProyectoSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Proyecto
+        load_instance = True
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
         load_instance = True

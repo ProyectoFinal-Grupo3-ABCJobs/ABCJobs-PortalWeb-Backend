502575672e0bb4a67520e3dce3ea72fe8caa3fc4
gcastrot82 <gcastrot82@hotmail.com>
2023-11-04 12:37:00 -0500

    Update: Se quita las relacion usuario empresa para manejo idependiente de los microservicios

--- gestion_autenticacion/modelo/modelousuarios.py
+++ gestion_autenticacion/modelo/modelousuarios.py
@@ -4,15 +4,15 @@ from sqlalchemy import Date
 
 db = SQLAlchemy()
 
-class Empresa(db.Model):
-    idEmpresa   = db.Column(db.Integer, primary_key = True)
-    razonSocial = db.Column(db.String(100))
-    nit         = db.Column(db.String(20))
-    direccion   = db.Column(db.String(100))
-    telefono    = db.Column(db.String(20))
-    idCiudad    = db.Column(db.Integer)
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), unique=True, nullable=True)
-    usuario = db.relationship('Usuario', back_populates='empresa', uselist=False)
+# class Empresa(db.Model):
+#     idEmpresa   = db.Column(db.Integer, primary_key = True)
+#     razonSocial = db.Column(db.String(100))
+#     nit         = db.Column(db.String(20))
+#     direccion   = db.Column(db.String(100))
+#     telefono    = db.Column(db.String(20))
+#     idCiudad    = db.Column(db.Integer)
+#     idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), unique=True, nullable=True)
+#     usuario = db.relationship('Usuario', back_populates='empresa', uselist=False)
     
 
 class Usuario(db.Model):
@@ -20,17 +20,17 @@ class Usuario(db.Model):
     usuario     = db.Column(db.String(50))
     contrasena  = db.Column(db.String(90))
     tipoUsuario = db.Column(db.String(100))
-    empresa = db.relationship('Empresa', back_populates='usuario', uselist=False)
+    # empresa = db.relationship('Empresa', back_populates='usuario', uselist=False)
    
 
 # Establecer la relación uno a uno entre Usuario y Empresa
-Usuario.empresa = db.relationship('Empresa', back_populates='usuario', uselist=False)
-Empresa.usuario = db.relationship('Usuario', back_populates='empresa')
+# Usuario.empresa = db.relationship('Empresa', back_populates='usuario', uselist=False)
+# Empresa.usuario = db.relationship('Usuario', back_populates='empresa')
 
-class EmpresaSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Empresa
-        load_instance = True
+# class EmpresaSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = Empresa
+#         load_instance = True
 
 
 class UsuarioSchema(SQLAlchemyAutoSchema):

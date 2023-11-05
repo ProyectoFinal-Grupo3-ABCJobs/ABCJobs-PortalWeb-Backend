240d06ab36c51a5c589c5a9985be5320b5867195
gcastrot82 <gcastrot82@hotmail.com>
2023-11-05 11:03:39 -0500

    Fix: Ajuste candidato sin Token para emparejamiento

--- gestion_autenticacion/app.py
+++ gestion_autenticacion/app.py
@@ -27,7 +27,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config["JWT_ALGORITHM"] = "HS256"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=10)
 app.config['PROPAGATE_EXEPTIONS'] = True
 app.config['JWT_HEADER_TYPE'] = ''
 

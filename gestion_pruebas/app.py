45a8a336d357739d824a42f318efb3c97de0bc4b
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-11-11 23:49:49 -0500

    Update: Nuevo microservicio gestion_pruebas

---
+++ gestion_pruebas/app.py
@@ -0,0 +1,52 @@
+from flask import Flask,jsonify
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from datetime import timedelta
+from dotenv import load_dotenv
+
+import os
+
+directorio_actual = os.getcwd()
+carpeta_actual = os.path.basename(directorio_actual)
+
+if carpeta_actual=='gestion_pruebas' or carpeta_actual=='app':
+    from vista.vista_pruebas import VistaSaludServicio,VistaConsultaPruebasCandidato
+    from modelo import db
+else:    
+    from gestion_pruebas.vista.vista_pruebas import VistaSaludServicio,VistaConsultaPruebasCandidato
+    from gestion_pruebas.modelo import db
+
+load_dotenv()
+
+app = Flask(__name__)
+
+if (os.getenv('DEV')=='1'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../proyectoABCJobs.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config["JWT_ALGORITHM"] = "HS256"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=30)
+app.config['PROPAGATE_EXEPTIONS'] = True
+
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+cors = CORS(app)
+
+api = Api(app)
+
+# Endpoints de pruebas
+api.add_resource(VistaConsultaPruebasCandidato,'/test/candidate/<int:id_candidato>')
+api.add_resource(VistaSaludServicio,'/test/ping')
+
+
+
+
+jwt = JWTManager(app)
+

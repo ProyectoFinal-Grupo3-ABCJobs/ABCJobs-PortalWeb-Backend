c1abab3ce51838611cd568941c96a6c44ce2a746
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-11-12 16:23:37 -0500

    Update: vista y endpoint entrevistas

--- gestion_pruebas/vista/vista_pruebas.py
+++ gestion_pruebas/vista/vista_pruebas.py
@@ -13,17 +13,22 @@ if carpeta_actual == "gestion_pruebas" or carpeta_actual == "app":
     from modelo import (
         db,
         Prueba,
+        Entrevista,
         PruebaSchema,
+        EntrevistaSchema
        
     )
 else:
     from gestion_pruebas.modelo import (
         db,
         Prueba,
+        Entrevista,
         PruebaSchema,
+        EntrevistaSchema
     )
 
 pruebas_schema = PruebaSchema()
+entrevista_schema = EntrevistaSchema()
 
 class VistaSaludServicio(Resource):
     def get(self):
@@ -38,6 +43,20 @@ class VistaConsultaPruebasCandidato(Resource):
         tokenPayload = get_jwt_identity()
         if tokenPayload["tipoUsuario"].upper() == "CANDIDATO":
             return "Hola"
+        else:
+            mensaje: dict = {
+                "mensaje 1313": "El token enviado no corresponde al perfil del usuario"
+            }
+            respuesta = jsonify(mensaje)
+            respuesta.status_code = 401
+            return respuesta
+        
+class VistaConsultaEntrevistasCandidato(Resource):
+    @jwt_required()
+    def get(self, id_candidato):
+        tokenPayload = get_jwt_identity()
+        if tokenPayload["tipoUsuario"].upper() == "CANDIDATO":
+            return "Entrevista"
         else:
             mensaje: dict = {
                 "mensaje 1313": "El token enviado no corresponde al perfil del usuario"
